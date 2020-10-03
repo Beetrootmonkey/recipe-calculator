c29faf1a8071b97f4d98215cbd40d14fcaac1241
Beetrootmonkey <n.adelhard@googlemail.com>
2020-10-03 16:21:26 +0200

    No longer using a database. Rewrote the minecraft mod that exported the data, and now the json file can be loaded directly into the RAM

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const itemRouter = require('./routes/items');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -7,5 +8,6 @@ app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // create a GET route
 app.get('/express_backend', (req, res) => {
-  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+  res.send({express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT'});
 });
+app.use('/items', itemRouter);
